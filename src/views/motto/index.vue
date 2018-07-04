757c4c9a6c7e23b120db2b40a069f56ac2315a75
Frère Jacques <zyb@zjex.com>
2018-07-04 10:28:22 +0800

    try

---
+++ src/views/motto/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="section">
+        <div class="container">
+            <h2>蝴蝶过山门</h2>
+            <div class="content">
+                <p>
+                    
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return { version: __PROJECT_VERSION__ };
+    },
+    install({viewRegistry}){
+        viewRegistry.addView({
+            path: '/motto',
+            name: 'motto',
+            handle: 'Motto',
+            order: 300,
+            component: this
+        });
+
+    }
+}
+</script>
+<style lang="scss">
+
+</style>
