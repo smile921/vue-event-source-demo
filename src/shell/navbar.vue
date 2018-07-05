4797c5675e1f27e8e777dc4ca4ea54f1932b32eb
Frère Jacques <zyb@zjex.com>
2018-07-05 09:32:32 +0800

    try to understand a little rx

--- src/shell/navbar.vue
+++ src/shell/navbar.vue
@@ -1,19 +1,4 @@
-<!--
-  - Copyright 2014-2018 the original author or authors.
-  -
-  - Licensed under the Apache License, Version 2.0 (the "License");
-  - you may not use this file except in compliance with the License.
-  - You may obtain a copy of the License at
-  -
-  -     http://www.apache.org/licenses/LICENSE-2.0
-  -
-  - Unless required by applicable law or agreed to in writing, software
-  - distributed under the License is distributed on an "AS IS" BASIS,
-  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  - See the License for the specific language governing permissions and
-  - limitations under the License.
-  -->
-
+ 
 <template>
   <nav id="navigation" class="navbar is-fixed-top">
     <div class="container">
@@ -59,7 +44,8 @@
   export default {
     data: () => ({
       showMenu: false,
-      brand: '<img src="assets/img/icon-spring-boot-admin.svg"><span>Spring Boot Admin</span>',
+      // brand: '<img src="assets/img/icon-spring-boot-admin.svg"><span>Custom Admin</span>',
+      brand: ' <span>Custom Admin</span>',
       userName: null
     }),
     props: {
--- src/views/motto/index.vue
+++ src/views/motto/index.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="section">
         <div class="container">
-            <h2>蝴蝶过山门</h2>
+            <h2 class="handwrite">蝴蝶过山门</h2>
             <div class="content">
-                <p>
-                    
+                <p class="hand-main">
+                 蝴蝶过山门，轻舟过重山。一别两宽，各生欢喜。解冤释结，更莫相憎。
                 </p>
             </div>
         </div>
@@ -29,5 +29,25 @@ export default {
 }
 </script>
 <style lang="scss">
+@font-face {
+  text-align: center;
+    width: 45%;
+    margin: 0 auto;
+
+  font-family: 'ZhuGe';
+  font-style: normal;
+  font-weight: 400;
+  src:  url(/assets/font/ZhuGe.ttf) format('truetype');
+//   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215, U+E0FF, U+EFFD, U+F000;
+}
+
+.handwrite{
+    font-size: 6em;
+    font-family: 'ZhuGe','Courier New', Courier, monospace;
+}
+.hand-main{    
+    font-size: 3em;
+    font-family: 'ZhuGe','Courier New', Courier, monospace;
+}
 
 </style>
